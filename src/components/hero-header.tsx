4ea9e851e80d976a983997817a76b22528ee38f7
damarketsecurities <damarketsecurities@gmail.com>
2026-07-07 11:42:46 +0800

    Update hero-header.tsx

--- src/components/hero-header.tsx
+++ src/components/hero-header.tsx
@@ -13,7 +13,7 @@ const menuItems = [
     { name: 'First News', href: '/first-news' },
     { name: 'FAQ', href: '/faq' },
     { name: 'Forms', href: '/forms' },
-    { name: 'Privacy Policy', href: '/privacy' },
+    { name: 'Privacy', href: '/privacy' },
 ]
 
 export const HeroHeader = () => {
